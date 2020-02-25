e2bb174386a5e49ca9314979573e6ac7f3052aa7
kamal_pci <kamal@portcities.net>
2020-02-25 09:13:01 +0700

    [ADD] custom many2manytags

---
+++ relation_field/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Custom Many2manyTags',
+    'version': '12.0.0.1.0',
+    'author': 'Portcities Ltd',
+    'website': 'https://www.portcities.net/',
+    'category': 'Marketing',
+    'summary': 'Custom Many2many Tags',
+    'description': '',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
